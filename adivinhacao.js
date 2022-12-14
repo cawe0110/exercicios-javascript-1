061552ccd5d171e650326b1302fb3452eddda9c2
cawe0110 <kaue_duarte@estudante.sc.senai.br>
2022-12-14 17:54:58 -0300

    commit3

--- adivinhacao.js
+++ adivinhacao.js
@@ -1,18 +1,26 @@
         //program intent
-        var number = parseInt(5)
-        var closeminus = parseInt(number-1)
-        var closeplus = parseInt(number+1)
-        var guess = parseInt(prompt("digite um numero entre 0 e 10"))
+        function skipline(){
+            document.write("<br><br>");
+        }
+        function show(frase){
+            document.write(frase);
+            skipline();
+        }
+
+        function adivinhe(){
+        var number = Math.round(Math.random() * 10);
+        var closeminus = parseInt(number-1);
+        var closeplus = parseInt(number+1);
+        var guess = parseInt(prompt("digite um numero entre 0 e 10"));
 
         if (guess == number){
             show("Voce Acertou!!!!")
         }
-        if (guess == number+1 || guess == number-1 ){
-            show("voce esta muito perto...")
-        }
-        if (guess == 10 || guess == 0){
-            show("errou feio errou rude")
-        }
-        else if(guess != 0 & 10 || guess != number){
-            show("errou")
-        }
+        else{
+            show("Errouu!!")
+        };
+
+        show("O numero é " + number); 
+        show("Você botou o numero "+ guess);     
+}
+
